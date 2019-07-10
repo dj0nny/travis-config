6eb93f434c5c1e4f2678c0df89b7f8768c82f4f6
dj0nny <frank96990@gmail.com>
2019-07-10 17:58:56 +0200

    add detection for existing .travis.yml, edit eslint rules

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
+const yaml = require('js-yaml');
 const inquirer = require('inquirer');
 
-console.log('work in progress');
+const fs = require('fs');
+const path = require('path');
+
+const existingconfig = fs.existsSync('.travis.yml');
+
+if (existingconfig) {
+  inquirer
+    .prompt([
+      {
+        type: 'confirm',
+        name: 'overwrite',
+        message: '.travis.yml alredy exists! Do you want of overwrite it?',
+        default: false,
+      },
+    ])
+    .then((answers) => {
+      if (answers.overwrite) {
+        console.log('overwrite');
+      } else {
+        process.exit(0);
+      }
+    });
+}
+// try {
+//   const travisFile = yaml.safeLoad(fs.readFileSync(path.join(process.cwd(), '.travis.yml'), 'utf8'));
+//   console.log(travisFile);
+// } catch (e) {
+//   console.log(e);
+// }
